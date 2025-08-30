3a1e7cace7d9ddbc08bd7e120aad1787fc125f06
Aduda Shem <100862586+Aduda-Shem@users.noreply.github.com>
2025-08-30 12:53:36 +0300

    ✨ initialized configs

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <template v-if="!isAuthRoute">
+      <AppShell>
+        <v-container class="py-6" fluid>
+          <router-view />
+        </v-container>
+      </AppShell>
+    </template>
+    <template v-else>
+      <router-view />
+    </template>
+  </v-app>
+</template>
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import AppShell from './components/layout/AppShell.vue'
+const route = useRoute()
+const isAuthRoute = computed(() => ['/login','/register'].includes(route.path))
+</script>
